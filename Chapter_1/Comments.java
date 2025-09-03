cd69c5854dc8e720627265e9f99b7e0ae069891a
ChrislynGonzales-UCM <chrislyngonzales144@gmail.com>
2025-09-03 20:47:32 +0800

    Update Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,1 +1,21 @@
+/**
+ * This is a documentation comment.
+ * It describes the class and its purpose.
+ * You can generate Javadoc from this type of comment.
+ */
+public class Comments {
+
+    public static void main(String[] args) {
+        // This is a single-line comment.
+        // It explains the next line of code.
+        System.out.println("Hello, this program demonstrates comments in Java!");
+
+        /* 
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         * Useful for explaining larger sections of code.
+         */
+        System.out.println("Java supports single-line, multi-line, and documentation comments.");
+    }
+}
 

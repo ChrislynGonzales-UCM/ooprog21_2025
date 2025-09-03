52043ca8fa354481e325fa1b572cdcec07c21f23
ChrislynGonzales-UCM <chrislyngonzales144@gmail.com>
2025-09-03 20:44:24 +0800

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,1 +1,10 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        
+        System.out.println("Line 1 of the song");
+        System.out.println("Line 2 of the song");
+        System.out.println("Line 3 of the song");
+        System.out.println("Line 4 of the song");
+    }
+}
 

ecb9686839c84037ca3481efcc6624def67b9a46
ChrislynGonzales-UCM <chrislyngonzales144@gmail.com>
2025-09-03 20:43:53 +0800

    Update FirstDialog.java

--- Chapter_1/FirstDialog.java
+++ Chapter_1/FirstDialog.java
@@ -1,1 +1,9 @@
+import javax.swing.JOptionPane; // Import class for dialogs
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "First Java dialog");
+    }
+}
 

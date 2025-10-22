7d0f68c095315fcf4c044e1055a336b45f10d5ad
ChrislynGonzales-UCM <chrislyngonzales144@gmail.com>
2025-10-22 09:57:40 +0300

    Update and rename BankInterest.java to BankBalance2.java

--- Chapter_6/BankBalance2.java
+++ Chapter_6/BankBalance2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BankInterest {
+public class BankBalance2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
